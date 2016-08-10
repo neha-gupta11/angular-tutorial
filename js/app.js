f777b48b783cb1ac1e955d3e35294541d9846cbb
Neha Gupta <neha.gupta@intelligrape.com>
2016-08-10 16:52:15 +0530

    Added example for controllers - which is not working;'

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+function simpleController($scope){
+    console.log("read");
+    $scope.customers=[
+        {name:'Dave Jones',city:'Pheonix'},
+        {name:'James Riley',city:'Atlanta'},
+        {name:'Heedy Wahlin',city:'Chandler'},
+        {name:'Thomas Winter',city:'Seattle'}
+    ];
+}
+
